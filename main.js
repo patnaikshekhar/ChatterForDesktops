64a22d27b822c6312e62189145c399e7f988cf90
Shekhar Patnaik <patnaikshekhar@gmail.com>
2015-11-09 20:01:57 +0000

    Basic electron app

---
+++ main.js
@@ -0,0 +1,33 @@
+/// <reference path="./typings/tsd.d.ts" />
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var mainWindow = null;
+var CONSTANTS = {
+    WIDTH: 800,
+    HEIGHT: 800
+};
+var params = {
+    debug: true
+};
+if (params.debug) {
+    require('crash-reporter').start();
+}
+app.on('windows-all-closed', function () {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({
+        width: CONSTANTS.WIDTH,
+        height: CONSTANTS.HEIGHT
+    });
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    if (params.debug) {
+        mainWindow.openDevTools();
+    }
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+});
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,42 @@
+/// <reference path="./typings/tsd.d.ts" />
+
+import app = require('app');
+import BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+let CONSTANTS = {
+	WIDTH: 800,
+	HEIGHT: 800
+};
+
+let params = {
+	debug: true	
+};
+
+if (params.debug) {
+	require('crash-reporter').start();
+}
+
+app.on('windows-all-closed', function() {
+	if (process.platform != 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('ready', function() {
+	mainWindow = new BrowserWindow({
+		width: CONSTANTS.WIDTH,
+		height: CONSTANTS.HEIGHT
+	})
+	
+	mainWindow.loadUrl('file://' + __dirname + '/index.html');
+	
+	if (params.debug) {
+		mainWindow.openDevTools();	
+	}
+	
+	mainWindow.on('closed', function() {
+		mainWindow = null;
+	});
+});
